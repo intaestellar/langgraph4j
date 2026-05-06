8971842475535ff487c8575d3412289a14fef160
intaeHwang <hwang9029@ckinform.com>
2026-05-06 11:29:15 +0900

    etc

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,17 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     implementation(platform("org.bsc.langgraph4j:langgraph4j-bom:1.8.14"))
     implementation("org.bsc.langgraph4j:langgraph4j-core")
+    implementation("org.bsc.langgraph4j:langgraph4j-langchain4j")
+
+    implementation(platform("dev.langchain4j:langchain4j-bom:1.13.0"))
+    implementation("dev.langchain4j:langchain4j")
+    implementation("dev.langchain4j:langchain4j-open-ai")
+    implementation("dev.langchain4j:langchain4j-http-client-jdk")
+
+    runtimeOnly("org.slf4j:slf4j-simple:2.0.13")
 }
 
 tasks.test {
